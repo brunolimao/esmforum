b9ba023cb73343cabbd3795e9d1f27887a4750fd
brunolimao <brunolima.2001@gmail.com>
2023-11-07 14:20:27 -0300

    parte 3

---
+++ testes/e2e/cypress/e2e/spec2.cy.js
@@ -0,0 +1,14 @@
+describe('Primeiro teste end-to-end', () => {
+  it('Cadastra uma pergunta e verifica se ela é listada', () => {
+    cy.visit('localhost:3000/resposta/1');
+    
+    cy.get('#textarea-resposta').type('6');
+    cy.get('#btn-resposta').click();
+    cy.get('#tabela-respostas').contains('6');
+  });
+});
+
+
+
+
+
